fe8c8609b69839d90a7847223f787f364c2a57f3
rabbitfighter <rabbitfighter@cryptolab.net>
2016-01-31 12:30:09 -0700

    Styles :)

--- status-codes.js
+++ status-codes.js
@@ -15,7 +15,7 @@ var server = http.createServer(function(request, response) {
   response.write("<!DOCTYPE html>");
   response.write("<html>");
   response.write("<head>");
-  response.write("<link href=\"../style.css\" rel=\"stylesheet\" type=\"text/css\"/>");
+  response.write("<style>@import url(http://fonts.googleapis.com/css?family=Lato:300,400,700); body{background: #fb887c; color: #fff; font-family: 'Lato', Arial, sans-serif;}h1{font-family: monospace; letter-spacing: -0.01em; font-weight: 700; font-style: normal; font-size: 60px; margin-left: -3px; line-height: 1em; color: #fff; text-align: center; margin-bottom: 8px; text-rendering: optimizeLegibility;}table{width: 80%; margin: auto;}a{margin-left: 10%; color: white} table, th, td{border: 1px solid #fff; border-collapse: collapse;}th, td{padding: 15px;}</style>");
   response.write("<title>HTTP STATUS CODES</title>");
   response.write("</head>");
   response.write("<body>");
